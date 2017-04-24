e0c3944e1c1e9e3756759837486c8d67ee522046
Ahmed Ahmed <eng.amostafa@gmail.com>
2017-04-23 23:54:24 -0400

    adding most of the sections

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log('hi');
+ $(document).ready(function(){       
+        var scroll_pos = 0;            
+        scroll_pos = $(this).scrollTop();
+		changeBackground(scroll_pos);        
+        $(document).scroll(function() {
+        	scroll_pos = $(this).scrollTop();
+        	changeBackground(scroll_pos)
+        });
+        
+});
+
+ function changeBackground(scroll_pos) {
+    if(scroll_pos > 50) {
+            $(".navbar").addClass('dark');
+        } else {
+            $(".navbar").removeClass('dark');
+    }
+ }
+
+
+// ( function( $ ) {
+//     // Init Skrollr
+//     var s = skrollr.init({
+//         render: function(data) {
+//             //Debugging - Log the current scroll position.
+//             //console.log(data.curTop);
+//         }
+//     });
+// } )( jQuery );
